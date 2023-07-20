3be0ded8e0c17fd406901657b56fe5fd552e1c28
kiran <nagukiran362@gmail.com>
2023-07-20 10:17:28 +0530

    First laravel commite

---
+++ resources/views/images.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Image Page</title>
+    <link rel="stylesheet" href="./images.css">
+</head>
+<body>
+    <h1>Images</h1>
+
+    <div class="image-container">
+        <img src="./images-images/butterfly.jpg" alt="Image 1">
+        <img src="./images-images/flower.jpg" alt="Image 2">
+        <img src="./images-images/marguerite.jpg" alt="Image 3">
+        <img src="./images-images/plant.jpg" alt="Image 4">
+        <img src="image5.jpg" alt="Image 5">
+    </div>
+</body>
+</html>
